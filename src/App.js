01d437af176d9da39c4d9f48a0e89c2b949aebbe
developer-abdulali <ab.ali.soomro@gmail.com>
2023-09-24 17:54:47 +0500

    deleted dummy data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [name, setName] = useState("dummyUser");
-  const [chats, setChats] = useState([
-    { name: "user1", message: "message1" },
-    { name: "dummyUser", message: "message2" },
-  ]);
+  const [name, setName] = useState("");
+  const [chats, setChats] = useState([]);
   const [msg, setMsg] = useState("");
   const sendChat = () => {
     const c = [...chats];
@@ -16,30 +13,39 @@ const App = () => {
   };
   return (
     <div>
-      <div>
-        <input type="text" placeholder="Enter your name to start chat" onBlur={e => setName(e.target.value)} />
-      </div>
-      <h3>User: {name}</h3>
-      <div className="chat-container">
-        {chats.map((c) => (
-          <div className={`container ${c.name === name ? "me" : ""}`}>
-            <p className="chatbox">
-              <strong>{c.name}: </strong>
-              <span>{c.message} </span>
-            </p>
+      {name ? null : (
+        <div>
+          <input
+            type="text"
+            placeholder="Enter your name to start chat"
+            onBlur={(e) => setName(e.target.value)}
+          />
+        </div>
+      )}
+      {name ? (
+        <div>
+          <h3>User: {name}</h3>
+          <div className="chat-container">
+            {chats.map((c) => (
+              <div className={`container ${c.name === name ? "me" : ""}`}>
+                <p className="chatbox">
+                  <strong>{c.name}: </strong>
+                  <span>{c.message} </span>
+                </p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-
-      <div className="btm">
-        <input
-          type="text"
-          value={msg}
-          onInput={(e) => setMsg(e.target.value)}
-          placeholder="enter your message..."
-        />
-        <button onClick={(e) => sendChat()}>Send</button>
-      </div>
+          <div className="btm">
+            <input
+              type="text"
+              value={msg}
+              onInput={(e) => setMsg(e.target.value)}
+              placeholder="enter your message..."
+            />
+            <button onClick={(e) => sendChat()}>Send</button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
