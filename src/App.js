b1976f7c7daedf7c8bd1199fe28a8d599646dfb1
developer-abdulali <ab.ali.soomro@gmail.com>
2023-09-24 17:43:51 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("dummyUser");
+  const [chats, setChats] = useState([
+    { name: "user1", message: "message1" },
+    { name: "dummyUser", message: "message2" },
+  ]);
+  const [msg, setMsg] = useState("");
+  const sendChat = () => {
+    const c = [...chats];
+    c.push({ name, message: msg });
+    setChats(c);
+    setMsg("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input type="text" placeholder="Enter your name to start chat" onBlur={e => setName(e.target.value)} />
+      </div>
+      <h3>User: {name}</h3>
+      <div className="chat-container">
+        {chats.map((c) => (
+          <div className={`container ${c.name === name ? "me" : ""}`}>
+            <p className="chatbox">
+              <strong>{c.name}: </strong>
+              <span>{c.message} </span>
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div className="btm">
+        <input
+          type="text"
+          value={msg}
+          onInput={(e) => setMsg(e.target.value)}
+          placeholder="enter your message..."
+        />
+        <button onClick={(e) => sendChat()}>Send</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
